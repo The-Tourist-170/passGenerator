542acf9fa89c5652ecfd383d6eb84b0a4b678eb0
Ayushya Saxena <ayushyasaxena170@gmail.com>
2024-07-30 05:13:29 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+
+  const passwordRef = useRef(null)
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+      
+    }
+
+    setPassword(pass)
+
+
+  }, [length, numberAllowed, charAllowed, setPassword])
+
+  useEffect(() => {passwordGenerator()}, [length, charAllowed, numberAllowed, passwordGenerator])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class="bg-slate-800 w-full max-w-2xl mx-auto my-4 rounded-lg text-center text-2xl px-6 py-6">
+        <div class="py-2 text-white">
+          Password Generator
+        </div>
+        <div class="rounded-lg shadow overflow-hidden flex">
+          <input 
+            type='text'
+            value={password}
+            class="w-full outline-none px-1 py-1"
+            placeholder='Password'
+            readOnly
+          />
+          <button class="bg-sky-700 text-white px-3 text-center text-2xl">
+            Ctrl+V
+          </button>
+        </div>
+        <div class="py-3 space-x-8 flex text-xl text-orange-500">
+          <div class="space-x-2">
+            <input 
+              type="range"
+              min={8}
+              max={100}
+              value={length}
+              class="cursor-pointer"
+              onChange={e => {setLength(e.target.value)}}
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div class="space-x-2">
+            <input 
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberAllowed"
+              onChange={() => {setNumberAllowed((prev) => !prev)}} 
+            />
+            <label htmlFor="numberAllowed">Numbers</label>
+          </div>
+          <div class="space-x-2">
+            <input 
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="charAllowed"
+              onChange={() => {setCharAllowed((prev) => !prev)}} 
+            />
+            <label htmlFor="charAllowed">Characters</label>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
